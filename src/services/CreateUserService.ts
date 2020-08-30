9eb44c5ac4fa7732cb48c13eac1e363ca18338f1
Natanael da silva Lima <taelima1997@gmail.com>
2020-08-30 11:50:36 -0300

    created hash password user

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -19,10 +20,12 @@ class CreateUserService {
       throw Error('this email already registered');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await userRepository.save(user);
