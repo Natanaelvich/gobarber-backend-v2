d07db792a12a457a0bd0ca35ee5653fb952f080d
Natanael da silva Lima <taelima1997@gmail.com>
2020-09-05 23:48:22 -0300

    configured cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -11,6 +12,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(upload.directory));
 app.use(routes);
