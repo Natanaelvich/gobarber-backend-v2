c29101ea71fcd868195b686590cee3334b988f09
Natanael <taelima1997@gmail.com>
2020-09-16 11:33:16 -0300

    integrated test with jest

---
+++ src/modules/Appointmensts/services/CreateAppointmentsService.spec.ts
@@ -0,0 +1,9 @@
+describe('CreateApppointment', () => {
+  it('should be able to create a new appoitment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  it('should not be able to create a new appoitment on the same time', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
