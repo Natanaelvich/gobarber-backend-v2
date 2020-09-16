56506f8e1f1b28d9205240a35cc24a432511bbe6
Natanael <taelima1997@gmail.com>
2020-09-16 12:12:59 -0300

    created test of create appointment

---
+++ src/modules/Appointmensts/repositories/FakeAppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import ICreateAppointmentDTO from '@modules/Appointmensts/dtos/ICreateAppointmentDTO';
+import IAppointmentsRepository from '@modules/Appointmensts/repositories/IAppointmentsRepository';
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+class FakeAppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(a => a.date === date);
+
+    return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    appointment.id = uuidv4();
+    appointment.provider_id = provider_id;
+    appointment.date = date;
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default FakeAppointmentsRepository;
--- src/modules/Appointmensts/services/CreateAppointmentsService.spec.ts
+++ src/modules/Appointmensts/services/CreateAppointmentsService.spec.ts
@@ -1,6 +1,20 @@
+import FakeAppointmentsRepository from '../repositories/FakeAppointmentsRepository';
+import CreateAppointmentsService from './CreateAppointmentsService';
+
 describe('CreateApppointment', () => {
-  it('should be able to create a new appoitment', () => {
-    expect(1 + 2).toBe(3);
+  it('should be able to create a new appoitment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+
+    const appoitment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appoitment).toHaveProperty('id');
+    expect(appoitment.provider_id).toBe('123123');
   });
 
   it('should not be able to create a new appoitment on the same time', () => {
