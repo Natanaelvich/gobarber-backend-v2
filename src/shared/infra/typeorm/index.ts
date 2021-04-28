30abf71b8904d0505e5053d75f2250f7a46d2c9e
Natanael Lima <taelima1997@gmail.com>
2021-04-27 23:07:47 -0300

    update server start

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,5 +1,21 @@
-import { createConnections } from 'typeorm';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-createConnections()
-  .then(() => console.log('connection database success'))
-  .catch(err => console.log('connection database failure', err));
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then(options => {
+  const newOptions = options as IOptions;
+  newOptions.host = 'mysql'; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({ ...options })
+    .then(() => console.log('connection database mysql 🎉'))
+    .catch(err => console.log('connection database mysql fail ', err));
+});
+
+getConnectionOptions().then(options => {
+  const newOptions = options as IOptions;
+  newOptions.host = 'mongodb'; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({ ...options })
+    .then(() => console.log('connection database mongo 🦎'))
+    .catch(err => console.log('connection database mongo fail ', err));
+});
